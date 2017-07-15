378ea8c03f1978c4deedcccfc3a13a93b7967c46
Saurabh Sharma <saurabhsharma@saurshaz.local>
2017-07-15 12:00:16 +0530

    fixed flow bugs

--- frontend/src/views/routes.js
+++ frontend/src/views/routes.js
@@ -2,18 +2,18 @@ import { Route, IndexRoute } from 'react-router';
 import Layout from '../views/Layout';
 import LoginPage from '../views/Login';
 import Clips from '../views/clips';
-import { requireAuth } from '../state-management/actions/authActions';
+// import { requireAuth } from '../state-management/actions/authActions';
 
-export default function Routes(store) {
-  const checkAdmin = (nextState, replace, callback) => {
-    const state = store.getState();
-    const auth = state.auth;
-
-    if (!auth.isLogged) {
-      store.dispatch(requireAuth(nextState, replace, callback));
-    }
-    callback();
-  };
+export default function Routes() {
+  // const checkAdmin = (nextState, replace, callback) => {
+  //   const state = store.getState();
+  //   const auth = state.auth;
+  //
+  //   if (!auth.isLogged) {
+  //     store.dispatch(requireAuth(nextState, replace, callback));
+  //   }
+  //   callback();
+  // };
 
   return (
     <Route
@@ -34,7 +34,6 @@ export default function Routes(store) {
       <Route
         component={Clips}
         path="/clips/:term"
-        onEnter={checkAdmin}
       />
     </Route>
   );
